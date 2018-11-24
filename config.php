5453462eb6debabd3508c2155d4d9adac29bf43e
Hania Maghfira <if.hania@outlook.com>
2018-11-24 17:17:49 +0700

    tampilkan titik loperman

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 	$host = "localhost";
 	$user = "postgres";
-	$pass = "1";
+	$pass = "56789";
 	$port = "5432";
 	$dbname = "loperman";
 	$conn = pg_connect("host=".$host." port=".$port."  dbname=".$dbname." user=".$user." password=".$pass) or  die("Koneksi gagal");
