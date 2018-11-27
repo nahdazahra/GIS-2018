8be2db87a45badc2d2721e0f0942fa9fc063a097
Hania Maghfira <if.hania@outlook.com>
2018-11-27 15:19:28 +0700

    UI tinggal hasil kolom cari

--- showwaypoint.php
+++ showwaypoint.php
@@ -2,7 +2,7 @@
 	header('Content-Type: application/json');
 	require('config.php');
 	$q=$_POST["routes"];
-	$querysearch="SELECT gid,nama,ST_X(ST_Centroid(geom)) AS lng, ST_Y(ST_CENTROID(geom)) As lat FROM titik where obj_id = '$q'";
+	$querysearch="SELECT gid,nama,ST_X(ST_Centroid(geom)) AS lng, ST_Y(ST_CENTROID(geom)) As lat FROM titik where obj_id = '$q' order by gid";
 	$hasil=pg_query($querysearch);
 	while($row = pg_fetch_array($hasil)){
 		$gid=$row['gid'];
