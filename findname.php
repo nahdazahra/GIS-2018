7bd724cc92a0f9e0b0ab1606eb9826d4b7f8a705
Hania Maghfira <if.hania@outlook.com>
2018-11-28 12:32:58 +0700

    udah yah gini aja wqwq

--- findname.php
+++ findname.php
@@ -2,7 +2,14 @@
 	header('Content-Type: application/json');
 	require('config.php');
 	$q=$_GET["q"];
-	$querysearch="SELECT gid,nama,ST_X(ST_Centroid(geom)) AS lng, ST_Y(ST_CENTROID(geom)) As lat FROM titik where upper(nama) like upper('%$q%')";
+	if ($q=="North Area") {
+		$q=1;
+	}
+	elseif ($q=="South Area") {
+		$q=2;
+	}
+	// $querysearch="SELECT gid,nama,ST_X(ST_Centroid(geom)) AS lng, ST_Y(ST_CENTROID(geom)) As lat FROM titik where upper(nama) like upper('%$q%')";
+	$querysearch="SELECT gid,nama,ST_X(ST_Centroid(geom)) AS lng, ST_Y(ST_CENTROID(geom)) As lat FROM titik where obj_id = '$q'";
 	$hasil=pg_query($querysearch);
 	while($row = pg_fetch_array($hasil)){
 		$gid=$row['gid'];
